241e4f9f606197f41856ea5001833b43a3ae447b
Daniel Lovasko <daniel.lovasko@gmail.com>
2017-11-02 22:56:13 +0000

    consistent argument ordering in empty function

--- slist.h
+++ slist.h
@@ -102,9 +102,9 @@
 /// Determine whether the list is empty.
 /// @return decision
 ///
-/// @param[in]  list list
 /// @param[out] out  decision
-#define SLIST_EMPTY(list, out)            \
+/// @param[in]  list list
+#define SLIST_EMPTY(out, list)            \
   do {                                    \
     *(out) = (SLIST_FIRST(list) == NULL); \
   } while (0)
